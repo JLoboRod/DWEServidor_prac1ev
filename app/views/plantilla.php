c0b66c6488386fe713cb8ed9f638eade8bdf4b3d
JLoboRod <jloborod@gmail.com>
2014-11-25 20:23:23 +0100

    Añadido Bootstrap 3.3.1 y JQuery 2.1.1 a los assets del proyecto

--- app/views/plantilla.php
+++ app/views/plantilla.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Vista</title>
+    <meta charset="utf-8">
+    <title>Vista</title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/DWEServidor_prac1ev/assets/css/bootstrap.min.css">
 </head>
 <body>
 	<?=CargarVista(BASE_DIR.'/views/encabezado.php');?>
 	<?=CargarVista(BASE_DIR.'/views/menu_navegacion.php');?>
 	<?=$htmlCuerpo?> Generado por el controlador previamente
 	<?=CargarVista(BASE_DIR.'/views/pie.php'); ?>
+
+    <!-- Los scripts aquí para acelerar la carga de la página -->
+    <script src="http://localhost/DWEServidor_prac1ev/assets/js/jquery-2.1.1.js"></script>
+    <script src="http://localhost/DWEServidor_prac1ev/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
 
