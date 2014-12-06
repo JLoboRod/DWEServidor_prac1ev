5271ce6bab0c2ea285e9944fd00c5ccaebeb6308
JLoboRod <jloborod@gmail.com>
2014-12-06 17:11:07 +0100

    Añadida documentación generada con ApiGen.

---
+++ doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","CargarVista()"],["c","controlador"],["f","CreaCheckBox()"],["f","CreaControlesFormulario()"],["f","CreaInputText()"],["f","CreaRadioButton()"],["f","CreaSelect()"],["c","Db"],["c","ModeloEnvios"],["c","ModeloProvincias"],["f","ValoresPost()"],["f","ValorGet()"],["f","ValorPost()"]];
